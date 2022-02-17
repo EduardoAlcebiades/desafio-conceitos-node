901685bb1b44fe64932177c4757cfe8573509c18
Eduardo <eduardoalcebiadesp@gmail.com>
2022-02-16 23:35:06 -0300

    functionality inclusion

---
+++ README.md
@@ -0,0 +1,57 @@
+# Fundamentos do NodeJS
+
+Neste projeto é abordado os principais fundamentos do [NodeJS](https://nodejs.org/en/) e testes utilizando [Jest](https://jestjs.io).
+
+## Descrição
+
+O projeto consiste em uma aplicação que gerencia tarefas (do inglês _todos_) para cada usuário.
+
+# Antes de começar
+
+Antes de iniciar o servidor é necessário instalar as dependências do NodeJS com o segunte comando:
+
+-- Utilizando NPM:
+```
+npm install
+```
+
+-- Ou YARN:
+```
+yarn
+```
+
+## Iniciando o Servidor
+
+Para iniciar o servidor, será necessário executar o seguinte comando:
+
+-- Utilizando NPM:
+```
+npm run dev
+```
+
+-- Ou YARN:
+```
+yarn dev
+```
+
+O servidor será inciado em http://localhost:3333.
+
+## Executando os testes
+
+Os testes estão disponíveis em "src/_\_tests__" e poderão ser executados executando o seguinte comando:
+
+-- Utilizando NPM:
+```
+npm run test
+```
+
+-- Ou YARN:
+```
+yarn test
+```
+
+# Prints do projeto
+
+![Create user request](https://github.com/EduardoAlcebiades/desafio-conceitos-node/blob/master/assets/images/create-user.jpg?raw=true)
+![Create todo request](https://github.com/EduardoAlcebiades/desafio-conceitos-node/blob/master/assets/images/create-todo.jpg?raw=true)
+![Tests running](https://github.com/EduardoAlcebiades/desafio-conceitos-node/blob/master/assets/images/tests.jpg?raw=true)
--- src/index.js
+++ src/index.js
@@ -1,41 +1,150 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user)
+    return response.status(401).json({
+      error: { message: "Unauthorized" },
+    });
+
+  request.user = user;
+
+  return next();
 }
 
-app.post('/users', (request, response) => {
-  // Complete aqui
+app.post("/users", (request, response) => {
+  const { name, username } = request.body;
+
+  const user = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: [],
+  };
+
+  const userExists = users.some((user) => user.username === username);
+
+  if (userExists)
+    return response.status(400).json({
+      error: {
+        message: "User already exists!",
+      },
+    });
+
+  users.push(user);
+
+  return response.status(201).json(user);
 });
 
-app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.get("/todos", checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+
+  return response.json(user.todos);
 });
 
-app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.post("/todos", checksExistsUserAccount, (request, response) => {
+  const { title, deadline } = request.body;
+  const { user } = request;
+
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date(),
+  };
+
+  user.todos.push(todo);
+
+  return response.status(201).json(todo);
 });
 
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+  const { title, deadline } = request.body;
+  const { id } = request.params;
+
+  let todoExists;
+
+  user.todos = user.todos.map((todo) => {
+    if (todo.id === id) {
+      todo = {
+        ...todo,
+        title: title || todo.title,
+        deadline: deadline ? new Date(deadline) : todo.deadline,
+      };
+
+      todoExists = todo;
+    }
+
+    return todo;
+  });
+
+  if (!todoExists)
+    return response.status(404).json({
+      error: {
+        message: "Todo not found!",
+      },
+    });
+
+  return response.status(201).json(todoExists);
 });
 
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+  const { id } = request.params;
+
+  let todoExists;
+
+  user.todos = user.todos.map((todo) => {
+    if (todo.id === id) {
+      todo.done = true;
+
+      todoExists = todo;
+    }
+
+    return todo;
+  });
+
+  if (!todoExists)
+    return response.status(404).json({
+      error: {
+        message: "Todo not found!",
+      },
+    });
+
+  return response.status(401).json(todoExists);
 });
 
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo)
+    return response.status(404).json({
+      error: {
+        message: "Todo not found!",
+      },
+    });
+
+  user.todos.splice(user, 1);
+
+  return response.status(204).json(todo);
 });
 
 module.exports = app;
